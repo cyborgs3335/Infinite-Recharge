96c88c50cd118c9991d9c8204357dfaba858f876
Lrogan <lrogan@arogan.com>
2020-02-17 20:14:59 -0600

    imports

--- Infinite Recharge/src/main/java/frc/robot/Robot.java
+++ Infinite Recharge/src/main/java/frc/robot/Robot.java
@@ -13,11 +13,7 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.triggerIntake;
-import frc.robot.subsystems.Dumper;
-import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.armControl;
-import frc.robot.subsystems.driveTrain;
-import frc.robot.subsystems.shooter;
+import frc.robot.subsystems.*;
 import frc.robot.subsystems.vision.Limelight;
 
 /**
