aba441c99675bbc0f9fff1a584254d1c23dba9ca
Lrogan <lrogan@arogan.com>
2020-03-01 16:33:20 -0600

    fixed heights

--- pls/src/main/java/frc/robot/RobotPreferences.java
+++ pls/src/main/java/frc/robot/RobotPreferences.java
@@ -16,14 +16,16 @@ public class RobotPreferences {
     //where constats for the robot are placed
     //TODO: set all constants in Inches
 
-    //arm constants                              //4096 ticks per revolution, 1 rev = 7d/6 inch
+    //arm constants  
+    public static final double armheight = 22.25;                     //4096 ticks per revolution, 1 rev = 7d/6 inch
     public static final double kWinchScalar = 4096/(7d/6);//done //ticks per inch of hight on the arm
         //height of arm constants
-    public static final double kControlBoardHeight = 30.5;//done
-    public static final double kFullExtendHeight = 78 +(7/8d);//done
+    public static final double kControlBoardHeight = 30.5 - armheight;//done
+    public static final double kFullExtendHeight = 78 +(7/8d) - armheight;//done
     public static final double kFullRetractHeight = 0;//done
-    public static final double kDefaultStart = 0;
-    public static final double kLowerExtendHeight = 22.24;//done
+    public static final double kDefaultStart = kFullRetractHeight;
+    public static final double kLowerExtendHeight = 22.24 - armheight ;//done
+    
 
 
     //speeds
